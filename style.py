3eee2a342deac876d9251cfb6fce907f4352bc66
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2021-05-28 00:41:05 -0300

    adicionado comentário

--- style.py
+++ style.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from index import main;
 
+
+
+# Executa o tinker, no qual e responsável pela parte gráfica. 
 root = Tk()
 root.title("IMC - Peso Corporal       versão 1.0.0")
 Application(root)
