2b0197c23c48e1d49b5671c337230b49649a419b
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2021-05-28 00:17:11 -0300

    adicionando função para calculo do IMC.
    código contêm à descrição.

--- index.py
+++ index.py
@@ -3,3 +3,16 @@ retornar = True
 # Seleciona se o modo desenvolvedor esta ligado.
 shell = True
 
+def imc(peso,altura):
+    '''
+        Calcula o IMC corporal do individuo.
+
+        Args:
+            peso(float): peso da individuo.
+            altura(float): peso da individuo.
+
+        Return:
+            calculo do IMC.
+    '''
+    return  peso / (altura * altura)
+
