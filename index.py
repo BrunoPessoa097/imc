8f219ca286d502198aa42985068287172f9657c0
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2021-05-28 00:19:50 -0300

    adicionando função tipologia
    saber a condição corporal do individuo.

--- index.py
+++ index.py
@@ -16,3 +16,25 @@ def imc(peso,altura):
     '''
     return  peso / (altura * altura)
 
+def tipologia(resultado):
+    '''
+        Retorna se o individuo está com sobrepeso ou não.
+
+        Args:
+            resultado(float): resultado do calculo do IMC.
+
+        Return:
+            retorno uma frase se a pessoa está com o peso ideal(frase).
+    '''
+
+    if resultado <= 18.5:
+        return "Magreza - obesidade tipo 0"
+    elif resultado > 18.5 or resultado <= 24.9:
+        return "Normal - Obesidade tipo 0"
+    elif resultado >= 25 or resultado <= 29.9:
+        return "Sobrepeso - Obesidade tipo 1"
+    elif resultado > 29.9 or resultado <= 39.9:
+        return "Obesidade - Obesidade tipo 2"
+    else:
+        return "Obesidade grave - Obesidade tipo 3"
+
